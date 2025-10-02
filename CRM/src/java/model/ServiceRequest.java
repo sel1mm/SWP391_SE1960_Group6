57f40d5f8e0cbbe9a2a1f7737f6ae50ddbf26c36
Doan Dinh Giap <doandinhgiap@gmail.com>
2025-10-02 15:10:19 +0700

    add entity model ver 1.1

---
+++ CRM/src/java/model/ServiceRequest.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.time.LocalDate;
+
+
+/**
+ *
+ * @author doand
+ */
+public class ServiceRequest {
+    private int requestId;
+    private int contractId;
+    private int equipmentId;
+    private int createdBy;
+    private String description;
+    private String priorityLevel = "Normal";
+    private LocalDate requestDate;
+    private String status;
+
+    public ServiceRequest() {
+    }
+
+    public ServiceRequest(int requestId, int contractId, int equipmentId, int createdBy, String description, LocalDate requestDate, String status) {
+        this.requestId = requestId;
+        this.contractId = contractId;
+        this.equipmentId = equipmentId;
+        this.createdBy = createdBy;
+        this.description = description;
+        this.requestDate = requestDate;
+        this.status = status;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(int contractId) {
+        this.contractId = contractId;
+    }
+
+    public int getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(int equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public LocalDate getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(LocalDate requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
