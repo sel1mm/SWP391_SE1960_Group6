b25a40e96d3a37e2e3fa36ad60da727f335560f3
Bachnh <nbach518@gmail.com>
2025-10-22 11:38:29 +0700

    donevalipassword

--- CRM/src/java/dal/DBContext.java
+++ CRM/src/java/dal/DBContext.java
@@ -14,9 +14,9 @@ public class DBContext {
         try {
             String username = "root";
 
-            String password = "sa12345";
+            String password = "sa123456";
 
-            String url = "jdbc:mysql://localhost:3306/reviewiter2?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+            String url = "jdbc:mysql://localhost:3306/reviewinter2?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
 
 
             Class.forName("com.mysql.cj.jdbc.Driver");
