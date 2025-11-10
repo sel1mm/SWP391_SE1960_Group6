b6952a75ac87912e37e6b00b5ba1bd3aaca5fd59
Phạm Quốc Bình <binhpqhe170203@fpt.edu.vn>
2025-11-10 23:18:55 +0700

    Technician: Repair Report v1.4.a
    minor update:
    - Fix DB merging conflicts
    - Update:
    repairreport functions estimatedCost logic (conversion) to help crms easier done Payment process.
    
    The lgic:
    DB is in USD.
    Display as VND.
    User input as VND.
    Form submission converse back to USD ( to store db ).
    
    - Binh.

--- CRM/src/java/dal/DBContext.java
+++ CRM/src/java/dal/DBContext.java
@@ -13,14 +13,9 @@ public class DBContext {
     public DBContext() {
         try {
             String username = "root";
-            String password = "1choigamenhuhack";
+            String password = "sa12345";
 
-<<<<<<< HEAD
-            String url = "jdbc:mysql://localhost:3306/testdb?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
-=======
-            String url = "jdbc:mysql://localhost:3306/swp?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
->>>>>>> 434d4a0168de59c59c1468fc151b44705f77543f
-
+String url = "jdbc:mysql://localhost:3306/swp?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
 
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, username, password);
