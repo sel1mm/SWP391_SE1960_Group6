f8bf5031a0434364e41abc313b39a7e4658ff78d
Doan Dinh Giap <doandinhgiap@gmail.com>
2025-11-12 21:17:39 +0700

    Update UI Invoices for Customer ver1

--- CRM/src/java/dal/DBContext.java
+++ CRM/src/java/dal/DBContext.java
@@ -18,7 +18,7 @@ public class DBContext {
 
             String password = "sa12345";
 
-            String url = "jdbc:mysql://localhost:3306/giap?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+            String url = "jdbc:mysql://localhost:3306/final?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
 
 
 
